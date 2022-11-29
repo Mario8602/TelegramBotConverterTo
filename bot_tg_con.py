59481679518049bf5157bf0d39e8c6dd6bbdf799
Mario8602 <hellp8901@gmail.com>
2022-11-29 18:38:07 +0500

    #2 add requirements, изменение в функциях абсолютных путей на относительные

--- bot_tg_con.py
+++ bot_tg_con.py
@@ -3,14 +3,7 @@ from random import randint
 import telebot
 from telebot import types
 
-import tempfile
-import uuid
-
-from nbconvert import HTMLExporter
-from nbformat.v2 import nbformat
-from pathlib import Path
-
-from reunion import pdf_to_mp3, photo_noir_convert, pdf_to_image, pdf_to_word, filesFolder, delete_file
+from reunion import pdf_to_mp3, photo_noir_convert, pdf_to_image, pdf_to_word, filesFolder, delete_file, create_folder
 import os
 
 bot = telebot.TeleBot("5780381393:AAHsbrC8uV8mib125ZucgCs6WxtNbZWPavE", parse_mode=None)
@@ -27,12 +20,12 @@ def start(message):
 def converter(message):
     # сохранение полученного файла и определение типа
     file_info = bot.get_file(message.document.file_id)
-    src = r"M:/PythonProjects/tg_bot_convert/received_file/" + message.document.file_name
+    print(file_info)
+    create_folder(file_name='received_file')
+    src = r"received_file/" + message.document.file_name
     file_download = bot.download_file(file_info.file_path)
     with open(src, 'wb') as new_file:
         new_file.write(file_download)
-    # filename, file_extension = os.path.splitext(file_info.file_path)
-    print(file_info.file_path)
 
     markup_inline = types.InlineKeyboardMarkup(row_width=1)
 
@@ -47,10 +40,10 @@ def converter(message):
 @bot.message_handler(content_types=['photo'])
 def converter(message):
     file_info = bot.get_file(message.photo[-1].file_id)
-    print(f'[+] {file_info}')
     file_downloaded = bot.download_file(file_info.file_path)
     file_namee = str(randint(1, 1000))
-    src = fr"M:\PythonProjects\tg_bot_convert\received_file\{file_namee}.jpeg"
+    create_folder(file_name='received_file')
+    src = fr"received_file\{file_namee}.jpeg"
     with open(src, 'wb') as new_file:
         new_file.write(file_downloaded)
 
@@ -66,6 +59,7 @@ def converter(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    """ В зависимости от выбранной кнопки, возвращает данные и вызывает необходимую функцию для обработки файла"""
     try:
         if call.message:
             method = call.data.split(',name=')[0]
@@ -74,43 +68,48 @@ def callback(call):
             if method == '2mp3':
 
                 bot.send_message(call.message.chat.id, 'Я В ПРОЦЕССЕ..., жди')
-
-                pdf_to_mp3(file_path=r"M:/PythonProjects/tg_bot_convert/received_file/" + file_name, language='ru')
-                file = open(fr'M:\PythonProjects\tg_bot_convert\mp_files\{file_name[:-4]}.mp3', 'rb')
+                create_folder(file_name='mp_files')
+                pdf_to_mp3(file_path=r"received_file/" + file_name, language='ru')
+                file = open(fr'mp_files\{file_name[:-4]}.mp3', 'rb')
 
                 bot.send_document(call.message.chat.id, file)
                 bot.send_message(call.message.chat.id, 'GOOD')
                 file.close()
 
             elif method == '2jpeg':
-                pdf_to_image(file_path=r"M:/PythonProjects/tg_bot_convert/received_file/" + file_name)
-                if filesFolder(r'M:\PythonProjects\tg_bot_convert\jpeg_files') == 1:
-                    file = open(fr'M:\PythonProjects\tg_bot_convert\jpeg_files\{file_name[:-4]}0001-1.jpg', 'rb')
+                create_folder(file_name='jpeg_files')
+                pdf_to_image(file_path=r"received_file/" + file_name)
+                if filesFolder(r'jpeg_files') == 1:
+                    file = open(fr'jpeg_files\{file_name[:-4]}0001-1.jpg', 'rb')
                     bot.send_document(call.message.chat.id, file)
                     file.close()
                 else:
-                    for page in range(filesFolder(r'M:\PythonProjects\tg_bot_convert\jpeg_files')):
-                        file = open(fr'M:\PythonProjects\tg_bot_convert\jpeg_files\{file_name[:-4]}0001-{page + 1}.jpg', 'rb')
+                    for page in range(filesFolder(r'jpeg_files')):
+                        file = open(fr'jpeg_files\{file_name[:-4]}0001-{page + 1}.jpg', 'rb')
                         bot.send_document(call.message.chat.id, file)
                         file.close()
                 bot.send_message(call.message.chat.id, 'оно?')
 
             elif method == '2word':
-                pdf_to_word(file_path="M:/PythonProjects/tg_bot_convert/received_file/" + file_name,
-                            docx_file=fr'M:\PythonProjects\tg_bot_convert\docx_files\{file_name[:-4]}.docx')
-                file = open(fr'M:\PythonProjects\tg_bot_convert\docx_files\{file_name[:-4]}.docx', 'rb')
+                create_folder(file_name='docx_files')
+                pdf_to_word(file_path=r"received_file/" + file_name,
+                            docx_file=fr'docx_files\{file_name[:-4]}.docx')
+                file = open(fr'docx_files\{file_name[:-4]}.docx', 'rb')
                 bot.send_document(call.message.chat.id, file)
                 file.close()
 
             elif method == 'blackout':
-                photo_noir_convert(file_path=fr"M:/PythonProjects/tg_bot_convert/received_file/{file_name}", file_name=file_name)
-                file = open(fr"M:\PythonProjects\tg_bot_convert\bw_photo\{file_name}", 'rb')
+                create_folder(file_name='bw_photo')
+                photo_noir_convert(file_path=fr"received_file/{file_name}", file_name=file_name)
+                file = open(fr"bw_photo\{file_name}", 'rb')
                 bot.send_document(call.message.chat.id, file)
 
             elif method == 'compress':
-                photo_noir_convert(file_path=fr"M:/PythonProjects/tg_bot_convert/received_file/{file_name}", file_name=file_name)
-                file = open(fr"M:\PythonProjects\tg_bot_convert\bw_photo\{file_name}", 'rb')
-                bot.send_document(call.message.chat.id, file)
+                create_folder(file_name='compress_files')
+                # create_folder(file_name='bw_photo')
+                # photo_noir_convert(file_path=fr"received_file/{file_name}", file_name=file_name)
+                # file = open(fr"bw_photo\{file_name}", 'rb')
+                # bot.send_document(call.message.chat.id, file)
 
             else:
                 bot.send_message(call.message.chat.id, 'oh, no, pls')
--- reunion.py
+++ reunion.py
@@ -10,53 +10,29 @@ from pdf2image import convert_from_path
 from pdf2docx import Converter
 
 
-def photo_noir_convert(file_path='test.jpg', file_name='test1.jpeg'):
-
-    """ Конвертировать изображение из цветного в чёрнобелое"""
-
-    a = np.asarray(Image.open(fp=file_path, mode='r'), dtype='uint8')
-    b = np.array([[[0.2989, 0.587, 0.114]]])
-    sums = np.round(np.sum(a * b, axis=2)).astype(np.uint8)
-    k = np.repeat(sums, 3).reshape(a.shape)
-    # file_name = Path(file_path).stem
-    Image.fromarray(k).save(fr"M:\PythonProjects\tg_bot_convert\bw_photo\{file_name}")
-
-
-# def main():
-#     print(photo_noir_convert(file_path=r"M:\PythonProjects\tg_bot_convert\received_file\9d0ecc90b315444a927f50b6eeaa91e6.jpeg"))
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
-def pdf_to_mp3(file_path='test.pdf', language='en'):
-
+def pdf_to_mp3(file_path='test.pdf', language='ru'):
     """ Конвертировать PDF-файл в mp3-файл"""
-
     if os.path.exists(file_path) and Path(file_path).suffix == '.pdf':
 
-        # print(f'[+] Processing...')
+        # открытие файла с использованием pdfplumber для получения подробных сведений о каждом текстовом символе
+        # extract - cобирает все символьные объекты страницы в одну строку
 
         with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
             pages = [page.extract_text() for page in pdf.pages]
-
         text = ''.join(pages)
-
         text = text.replace('\n', '')
+
+        # преобразования текста в речь Google Translate
 
         my_audio = gTTS(text=text, lang=language, slow=False)
         file_name = Path(file_path).stem
-        my_audio.save(fr'M:\PythonProjects\neiron\mp_files\{file_name}.mp3')
-
-        # return f'[+] {file_name}.mp3 saved success'
+        my_audio.save(fr'mp_files\{file_name}.mp3')
         return f'{file_name}.mp3'
     else:
         return FileNotFoundError
 
 
 # def main():
-#     tprint('PDF >> TO >> MP3', "white_bubble")
 #     file_path = input("\nУкажите путь до файла: ")
 #     language = input("\nУкажите язык: ")
 #     print(pdf_to_mp3(file_path=file_path, language=language))
@@ -68,14 +44,12 @@ def pdf_to_mp3(file_path='test.pdf', language='en'):
 
 def pdf_to_image(file_path='test.pdf'):
 
-    output_folder = r'M:\PythonProjects\neiron\pdftojpeg'
-
+    output_folder = r'jpeg_files/'
     file_name = Path(file_path).stem
-    images = convert_from_path(fr'M:\PythonProjects\neiron\pdf_files\{file_name}.pdf',
+    images = convert_from_path(fr'jpeg_files\{file_name}.pdf',
                                poppler_path=r'M:\poppler-22.04.0\Library\bin',
                                output_folder=output_folder, fmt='jpeg',
                                output_file=f'{file_name}')
-
     return f'[+] {file_name}.jpg saved succes!'
 
 
@@ -88,6 +62,7 @@ def pdf_to_image(file_path='test.pdf'):
 
 
 def pdf_to_word(file_path='test.pdf', docx_file='test.docx'):
+    """ Конвертировать PDF-файл в word-файл"""
     file = Converter(file_path)
     file.convert(docx_file)
     file.close()
@@ -101,27 +76,49 @@ def pdf_to_word(file_path='test.pdf', docx_file='test.docx'):
 # if __name__ == "__main__":
 #     main()
 
-def filesFolder(file_path1=r"M:\PythonProjects\neiron\mp_files"):
+
+def filesFolder(file_path1=r"mp_files"):
     for root, dirs, files in os.walk(file_path1):
         return len(files)
 
 
+def delete_file():
+    """Очистка файлов из списка папок"""
+    try:
+        for i in ['received_file', 'mp_files', 'docx_files', 'jpeg_files', 'bw_photo', '']:
+            for root, dirs, files in os.walk(fr"{i}"):
+                for filename in files:
+                    os.remove(fr'{i}\{filename}')
+    except Exception as ex:
+        return ex
+
+
+def create_folder(file_name='testfile'):
+    """Создание папки в директории, для файлов полученных тг-ботом, если папки не существует"""
+    try:
+        new_path = Path(file_name)
+        if Path.is_dir(new_path):
+            return 'No folder'
+        return Path.mkdir(new_path)
+    except Exception as ex:
+        return ex
+
+
+def photo_noir_convert(file_path='test.jpg', file_name='test1.jpeg'):
+    """ Конвертировать изображение из цветного в чёрнобелое"""
+    a = np.asarray(Image.open(fp=file_path, mode='r'), dtype='uint8')
+    b = np.array([[[0.2989, 0.587, 0.114]]])
+    sums = np.round(np.sum(a * b, axis=2)).astype(np.uint8)
+    k = np.repeat(sums, 3).reshape(a.shape)
+    Image.fromarray(k).save(fr"bw_photo\{file_name}")
+
+
 # def main():
-#     print(filesFolder(r'M:\PythonProjects\neiron\docx_files'))
+#     print(photo_noir_convert(file_path=r"M:\PythonProjects\tg_bot_convert\received_file\9d0ecc90b315444a927f50b6eeaa91e6.jpeg"))
 #
 #
 # if __name__ == "__main__":
 #     main()
-
-
-def delete_file():
-    try:
-        for i in ['mp_files', 'docx_files', 'pdf_files', 'pdftojpeg']:
-            for root, dirs, files in os.walk(fr"M:\PythonProjects\neiron\{i}"):
-                for filename in files:
-                    os.remove(fr'M:\PythonProjects\neiron\{i}\{filename}')
-    except Exception as ex:
-        return ex
 
 
 def photo_oua():
@@ -129,6 +126,7 @@ def photo_oua():
 
 
 def mourin(n):
+    """ Список простых чисел до n """
     c = 0
     result = []
     for i in range(1, n+1):
@@ -149,5 +147,3 @@ def check_number(number=11):
 
 def compress():
     pass
-
-
